dd4dbd62015f7fdecab6f604a690cf2ef81292e9
Arnaud Lenglet <arlenglet@gmail.com>
2015-07-12 00:32:29 +0200

    feat(Home): improve animation on home page + call to action buttons
    Former-commit-id: cfabf45dbe2fb5a2b16c1affba17802a8b1a4098

--- source/javascripts/flying-robot-projects.js
+++ source/javascripts/flying-robot-projects.js
@@ -4,4 +4,27 @@ $(function() {
     $('html,body').animate({scrollTop: $(".home-field").offset().top}, 800);
   });
 
+  var animationTime = 200,
+      homeIcones = [".balloon", ".zeppelin", ".plane", ".drone", ".rocket"];
+
+  function homePageLoadingAnimation(array) {
+    if (array.length > 0) {
+      $( array[0] ).animate({
+        opacity: 1
+      }, animationTime, function() {
+        array.shift();
+        homePageLoadingAnimation(array);
+      });
+    } else {
+      $( ".call-to-action-buttons" ).animate({
+        opacity: 1,
+        margin: "+=30px auto 0"
+      }, 1200, function() {
+        console.log('OK');
+      });
+    }
+  }
+
+  homePageLoadingAnimation(homeIcones);
+
 });
