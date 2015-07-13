ea4af42aebdcaa557ee8a2f19c37893e2345b916
Arnaud Lenglet <arlenglet@gmail.com>
2015-07-13 23:22:22 +0200

    feat(home): add mask layer on step mouseover
    Former-commit-id: befb9923bf24917af36a04169fec8c88ae40c2f4

--- source/javascripts/flying-robot-projects.js
+++ source/javascripts/flying-robot-projects.js
@@ -6,6 +6,15 @@ $(function() {
   var animationTime = 200,
       homeIcones = [".balloon", ".zeppelin", ".plane", ".drone", ".rocket"];
 
+  $('.steps-icons-container').find('.step').mouseover(function() {
+    $('.mask').addClass('active');
+    $(this).find('.hover-flag').addClass('visible');
+  });
+  $('.steps-icons-container').find('.step').mouseout(function() {
+    $('.mask').removeClass('active');
+    $(this).find('.hover-flag').removeClass('visible');
+  });
+
   function homePageLoadingAnimation(array) {
     if (array.length > 0) {
       $('.steps-icons-container').find(array[0]).animate({
@@ -23,47 +32,37 @@ $(function() {
   // I know, it's a shame. I'll refactor this "quick & horrible dirty" thing soon...
 
   $('.steps-icons-container').find('.balloon').mouseover(function() {
-    $( this ).find('.hover-flag').addClass('visible');
     $( this ).parent().parent().find('.step-descriptions-container').find('.balloon').addClass('visible');
   });
   $('.steps-icons-container').find('.balloon').mouseout(function() {
-    $( this ).find('.hover-flag').removeClass('visible');
     $( this ).parent().parent().find('.step-descriptions-container').find('.balloon').removeClass('visible');
   });
 
   $('.steps-icons-container').find('.zeppelin').mouseover(function() {
-    $( this ).find('.hover-flag').addClass('visible');
     $( this ).parent().parent().find('.step-descriptions-container').find('.zeppelin').addClass('visible');
   });
   $('.steps-icons-container').find('.zeppelin').mouseout(function() {
-    $( this ).find('.hover-flag').removeClass('visible');
     $( this ).parent().parent().find('.step-descriptions-container').find('.zeppelin').removeClass('visible');
   });
 
   $('.steps-icons-container').find('.plane').mouseover(function() {
-    $( this ).find('.hover-flag').addClass('visible');
     $( this ).parent().parent().find('.step-descriptions-container').find('.plane').addClass('visible');
   });
   $('.steps-icons-container').find('.plane').mouseout(function() {
-    $( this ).find('.hover-flag').removeClass('visible');
     $( this ).parent().parent().find('.step-descriptions-container').find('.plane').removeClass('visible');
   });
 
   $('.steps-icons-container').find('.drone').mouseover(function() {
-    $( this ).find('.hover-flag').addClass('visible');
     $( this ).parent().parent().find('.step-descriptions-container').find('.drone').addClass('visible');
   });
   $('.steps-icons-container').find('.drone').mouseout(function() {
-    $( this ).find('.hover-flag').removeClass('visible');
     $( this ).parent().parent().find('.step-descriptions-container').find('.drone').removeClass('visible');
   });
 
   $('.steps-icons-container').find('.rocket').mouseover(function() {
-    $( this ).find('.hover-flag').addClass('visible');
     $( this ).parent().parent().find('.step-descriptions-container').find('.rocket').addClass('visible');
   });
   $('.steps-icons-container').find('.rocket').mouseout(function() {
-    $( this ).find('.hover-flag').removeClass('visible');
     $( this ).parent().parent().find('.step-descriptions-container').find('.rocket').removeClass('visible');
   });
 
